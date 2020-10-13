6833de9914c3204be628fdb9ecf21f53d55119c0
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2020-10-13 15:09:58 +0200

    Extracted MyApp into new File and renamed to TimerProgressCalculatorApp
    Signed-off-by: Andreas Fahrecker <AndreasFahrecker@gmail.com>

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:time_progress_calculator/models/app_state.dart';
+import 'package:time_progress_calculator/screens/progress_screen.dart';
+
+class TimeProgressCalculatorApp extends StatelessWidget {
+  final Store<AppState> store;
+
+  TimeProgressCalculatorApp({Key key, this.store}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider(
+      store: store,
+      child: MaterialApp(
+        title: "Time Progress Calculator",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        initialRoute: "/",
+        routes: {
+          "/": (BuildContext context) => ProgressScreen(name: "Zivildienst")
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:time_progress_calculator/app.dart';
+import 'package:time_progress_calculator/models/app_state.dart';
+import 'package:time_progress_calculator/reducers/app_state_reducer.dart';
 import 'package:time_progress_calculator/screens/progress_screen.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Progress Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: "/",
-      routes: {
-        "/": (BuildContext context) => ProgressScreen(
-              name: "Zivildienst",
-            )
-      },
-    );
-  }
+  runApp(TimeProgressCalculatorApp(
+    store: Store<AppState>(appStateReducer, initialState: AppState.initial()),
+  ));
 }
