86862a9d9128a5181e00275dae403250c307e142
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2020-10-13 15:05:23 +0200

    Created timer_selectors with a selector for all timers
    Signed-off-by: Andreas Fahrecker <AndreasFahrecker@gmail.com>

---
+++ lib/selectors/timer_selectors.dart
@@ -0,0 +1,4 @@
+import 'package:time_progress_calculator/models/app_state.dart';
+import 'package:time_progress_calculator/models/timer.dart';
+
+List<Timer> timersSelector(AppState state) => state.timers;
