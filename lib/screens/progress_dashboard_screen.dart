142b482047634b8eb8910d020c873aeff5b21a8a
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2020-12-04 04:05:41 +0100

    Dashboard Now Shows started and future times.
    These cardss are divides based on their count
    
    Signed-off-by: Andreas Fahrecker <AndreasFahrecker@gmail.com>

--- lib/screens/progress_dashboard_screen.dart
+++ lib/screens/progress_dashboard_screen.dart
@@ -16,10 +16,12 @@ class ProgressDashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AppBar appBar = AppBar(
+      title: Text(title),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+      appBar: appBar,
       drawer: AppDrawer(),
       body: StoreConnector(
         converter: _ViewModel.fromStore,
@@ -30,11 +32,11 @@ class ProgressDashboardScreen extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           }
-          List<Widget> dashboardTileList = List<Widget>();
 
-          if (vm.startedTimeProgreses.length > 0) {
+          List<Widget> startedProgressesTileList = List<Widget>();
+          if (vm.hasStartedProgresses) {
             for (TimeProgress tp in vm.startedTimeProgreses) {
-              dashboardTileList.add(
+              startedProgressesTileList.add(
                 Card(
                   child: ListTile(
                     title: Text(tp.name),
@@ -54,20 +56,82 @@ class ProgressDashboardScreen extends StatelessWidget {
                 ),
               );
             }
-          } else {
-            dashboardTileList.add(ListTile(
-              title: Text("You don't have any tracked Progress."),
+          }
+
+          List<Widget> futureProgressesTileList = List<Widget>();
+          if (vm.hasFutureProgresses) {
+            for (TimeProgress tp in vm.futureTimeProgresses) {
+              futureProgressesTileList.add(
+                Card(
+                  child: ListTile(
+                    title: Text(tp.name),
+                    subtitle: Text(
+                        "Starts in ${tp.startTime.difference(DateTime.now()).inDays} Days."),
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, ProgressDetailScreen.routeName,
+                          arguments: ProgressDetailScreenArguments(tp.id));
+                    },
+                  ),
+                ),
+              );
+            }
+          }
+
+          double dividerHeight = 1;
+          double screenHeight = MediaQuery.of(context).size.height -
+              appBar.preferredSize.height -
+              24 -
+              dividerHeight; //Divider
+
+          List<Widget> columnChildren = List<Widget>();
+          int tpCount =
+              vm.startedTimeProgreses.length + vm.futureTimeProgresses.length;
+          if (vm.hasStartedProgresses) {
+            columnChildren.add(Container(
+              height: vm.hasFutureProgresses
+                  ? (screenHeight / tpCount) * vm.startedTimeProgreses.length
+                  : screenHeight,
+              child: ListView(
+                padding: EdgeInsets.all(8),
+                children: startedProgressesTileList,
+              ),
+            ));
+          }
+          if (vm.hasStartedProgresses && vm.hasFutureProgresses) {
+            columnChildren.add(Divider(
+              height: dividerHeight,
+            ));
+          }
+          if (vm.hasFutureProgresses) {
+            columnChildren.add(Container(
+              height: vm.hasStartedProgresses
+                  ? (screenHeight / tpCount) * vm.futureTimeProgresses.length
+                  : screenHeight,
+              child: ListView(
+                padding: EdgeInsets.all(8),
+                children: futureProgressesTileList,
+              ),
             ));
           }
 
-          return ListView(
-            padding: EdgeInsets.all(8),
-            children: dashboardTileList,
+          if (!vm.hasStartedProgresses && !vm.hasFutureProgresses) {
+            columnChildren.add(Container(
+              margin: EdgeInsets.all(16),
+              child: Center(
+                child: Text("You don't have any tracked Progress."),
+              ),
+            ));
+          }
+
+          return Column(
+            children: columnChildren,
           );
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
+        heroTag: "createProgressBTN",
         child: Icon(Icons.add),
         onPressed: () {
           Navigator.pushNamed(context, ProgressCreationScreen.routeName);
@@ -79,19 +143,29 @@ class ProgressDashboardScreen extends StatelessWidget {
 
 class _ViewModel {
   final List<TimeProgress> startedTimeProgreses;
+  final bool hasStartedProgresses;
   final List<TimeProgress> futureTimeProgresses;
+  final bool hasFutureProgresses;
   final bool hasLoaded;
 
   _ViewModel({
     @required this.startedTimeProgreses,
+    @required this.hasStartedProgresses,
     @required this.futureTimeProgresses,
+    @required this.hasFutureProgresses,
     @required this.hasLoaded,
   });
 
   static _ViewModel fromStore(Store<AppState> store) {
+    List<TimeProgress> startedTPList =
+        startedTimeProgressesSelector(store.state);
+    List<TimeProgress> furtureTPList =
+        futureTimeProgressesSelector(store.state);
     return _ViewModel(
-      startedTimeProgreses: startedTimeProgressesSelector(store.state),
-      futureTimeProgresses: futureTimeProgressesSelector(store.state),
+      startedTimeProgreses: startedTPList,
+      hasStartedProgresses: startedTPList.length > 0,
+      futureTimeProgresses: furtureTPList,
+      hasFutureProgresses: furtureTPList.length > 0,
       hasLoaded: store.state.hasLoaded,
     );
   }
