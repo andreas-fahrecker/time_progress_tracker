06b1c3cc70a511594bf8c1fdbafc200284b7eebd
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2021-02-24 01:43:25 +0100

    Created Load and Update Settings reducers
    Signed-of-by : Andreas Fahrecker <AndreasFahrecker@gmail.com>

--- lib/reducers/app_state_reducer.dart
+++ lib/reducers/app_state_reducer.dart
@@ -1,3 +1,6 @@
+import 'package:redux/redux.dart';
+import 'package:time_progress_tracker/actions/actions.dart';
+import 'package:time_progress_tracker/models/app_settings.dart';
 import 'package:time_progress_tracker/models/app_state.dart';
 import 'package:time_progress_tracker/reducers/has_loaded_reducer.dart';
 import 'package:time_progress_tracker/reducers/time_progress_list_reducer.dart';
@@ -8,3 +11,16 @@ AppState appStateReducer(AppState state, dynamic action) {
     timeProgressList: timeProgressListReducer(state.timeProgressList, action),
   );
 }
+
+final appSettingsReducers = combineReducers<AppSettings>([
+  TypedReducer<AppSettings, AppSettingsLoadedActions>(_loadAppSettings),
+  TypedReducer<AppSettings, UpdateAppSettingsActions>(_updateAppSettings)
+]);
+
+AppSettings _loadAppSettings(
+        AppSettings appSettings, AppSettingsLoadedActions nS) =>
+    nS.appSettings;
+
+AppSettings _updateAppSettings(
+        AppSettings appSettings, UpdateAppSettingsActions nS) =>
+    nS.appSettings;
