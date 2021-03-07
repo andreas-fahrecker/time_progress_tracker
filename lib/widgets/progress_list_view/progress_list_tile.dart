cebb9a8b53b2cfac3642f9e24a1f0b828e87d93e
Andreas Fahrecker <AndreasFahrecker@gmail.com>
2021-03-07 19:18:01 +0100

    Created ProgressListTileStrings class
    Signed-off-by: Andreas Fahrecker <AndreasFahrecker@gmail.com>

--- lib/widgets/progress_list_view/progress_list_tile.dart
+++ lib/widgets/progress_list_view/progress_list_tile.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:time_progress_tracker/models/time_progress.dart';
 
+class ProgressListTileStrings {
+  static String percentString(TimeProgress tp) =>
+      "${(tp.percentDone() * 100).floorToDouble()} %";
+
+  static String startsInDaysString(TimeProgress tp) =>
+      "Starts in ${tp.daysTillStart()} Days.";
+
+  static String endedDaysAgoString(TimeProgress tp) =>
+      "Ended ${tp.daysSinceEnd()} Days ago.";
+}
+
 class ProgressListTile extends StatelessWidget {
   final TimeProgress timeProgress;
   final Color doneColor, leftColor;
@@ -12,20 +23,13 @@ class ProgressListTile extends StatelessWidget {
     @required this.leftColor,
   });
 
-  String percentString() =>
-      "${(this.timeProgress.percentDone() * 100).floorToDouble()} %";
-
-  String startsInDaysString() =>
-      "Starts in ${timeProgress.daysTillStart()} Days.";
-
-  String endedDaysAgoString() =>
-      "Ended ${timeProgress.daysSinceEnd()} Days ago.";
-
   Widget _renderSubtitle(BuildContext context) {
-    if (!timeProgress.hasStarted()) return Text(startsInDaysString());
-    if (timeProgress.hasEnded()) return Text(endedDaysAgoString());
+    if (!timeProgress.hasStarted())
+      return Text(ProgressListTileStrings.startsInDaysString(timeProgress));
+    if (timeProgress.hasEnded())
+      return Text(ProgressListTileStrings.endedDaysAgoString(timeProgress));
     return LinearPercentIndicator(
-      center: Text(percentString()),
+      center: Text(ProgressListTileStrings.percentString(timeProgress)),
       percent: timeProgress.percentDone(),
       progressColor: doneColor,
       backgroundColor: leftColor,
